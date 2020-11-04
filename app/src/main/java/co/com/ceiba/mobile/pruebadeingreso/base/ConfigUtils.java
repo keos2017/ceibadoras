79e77b75ab8ba7a90ef1399f46c29b024131b6a9
Zorayda <zgutierrez@ikatechsolutions.com>
2020-11-04 00:09:26 -0500

    Al probar los endpoints se notó que todo venía en minúscula, por lo cuál se crean métodos validadores de textos necesarios

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/base/ConfigUtils.java
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/base/ConfigUtils.java
@@ -1,4 +1,38 @@
 package co.com.ceiba.mobile.pruebadeingreso.base;
 
 public class ConfigUtils {
+
+    /**
+     * 
+     * @param enteredText
+     * @return si el texto pasó correctamente por las validaciones
+     */
+    public static boolean validateString(String enteredText) {
+        return enteredText != null &&
+                !enteredText.isEmpty() &&
+                !enteredText.equalsIgnoreCase("null") &&
+                !enteredText.equalsIgnoreCase("0") &&
+                !enteredText.equalsIgnoreCase("0.0");
+    }
+    
+    /**
+     * 
+     * @param enteredText texto a validar y convertir
+     * @return texto con la letra inicial mayúscula y el resto en minúscula
+     */
+    public static String putCapitalInitial(String enteredText) {
+
+        if (validateString(enteredText)) {
+
+            String textProcessed = enteredText.toLowerCase();
+            char[] chapters = textProcessed.toCharArray();
+
+            chapters[0] = Character.toUpperCase(chapters[0]);
+            textProcessed = String.valueOf(chapters);
+
+            return textProcessed;
+        } else {
+            return "Texto vacío";
+        }
+    }
 }
