fa046e932932d65c6ecb94ccbfd0c00f643195cc
Zorayda <zgmona15@gmail.com>
2020-11-04 02:42:13 -0500

    Validación de lista de post vacías

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/post/view/PostActivity.java
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/post/view/PostActivity.java
@@ -2,16 +2,22 @@ package co.com.ceiba.mobile.pruebadeingreso.post.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ViewSwitcher;
 
 import co.com.ceiba.mobile.pruebadeingreso.R;
 
 public class PostActivity extends Activity {
 
+    ViewSwitcher mListViewSwitcher;
+    RecyclerView mListRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        initializeComponents();
     }
 
     @Override
@@ -19,5 +25,17 @@ public class PostActivity extends Activity {
         super.onStart();
     }
 
+    private void initializeComponents() {
+        mListViewSwitcher = findViewById(R.id.viewSwitcherList);
+    }
 
+    private void validationEmptyList(boolean isEmpty) {
+        if (!isEmpty) {
+            mListViewSwitcher.setDisplayedChild(0);
+            mListRecyclerView.setVisibility(View.VISIBLE);
+        } else {
+            mListRecyclerView.setVisibility(View.GONE);
+            mListViewSwitcher.setDisplayedChild(1);
+        }
+    }
 }
